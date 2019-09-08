7b9486bd4c29ac86dd8e59047d88caa9d240e8d9
Xue <1099232082@qq.com>
2019-09-08 21:22:23 +0800

    commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,45 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Button, Input, List } from 'antd'
 import axios from 'axios'
 import store from '../store/index' // 在组件中引入store
-import { 
-  changeInputAction, 
-  addItemAction, 
-  deleteItemAction, 
-  getListAction 
+import {
+  changeInputAction,
+  addItemAction,
+  deleteItemAction,
+  getListAction
 } from '../store/actionCreators'
 
 class TodoList extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = store.getState() // 从store中获取state数据
     store.subscribe(this.storeChange) // 订阅Redux的状态
   }
 
+  /**
+   * 当订阅的redux状态变化时，使用setState()方法，将新的数据存入state中。
+   */
   storeChange = () => {
     this.setState(store.getState())
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // 获取远程接口数据
-    axios.get('https://www.easy-mock.com/mock/5d74ebc59b07047ee5315081/example/redux/va/list')
-    .then((res)=>{
-      const data = res.data
-      const action = getListAction(data)
-      store.dispatch(action)
-    })
+    axios.get('http://rap2api.taobao.org/app/mock/230515/list')
+      .then((res) => {
+        const data = res.data
+        const action = getListAction(data)
+        store.dispatch(action)
+      })
   }
 
+  /**
+   * 处理输入框内容变化
+   */
   changeInputValue = (e) => {
-    const action = changeInputAction(e.target.value) // action是一个对象
+    const action = changeInputAction(e.target.value) // action是一个对象{type, value}
     store.dispatch(action) // 通过dispatch()方法将action传递给store
   }
 
+  /**
+   * 处理点击增加按钮
+   */
   clickBtn = () => {
     const action = addItemAction()
     store.dispatch(action)
   }
 
+  /**
+   * 处理点击删除按钮
+   */
   deleteItem = (index) => {
     const action = deleteItemAction(index)
     store.dispatch(action)
@@ -79,8 +91,8 @@ class TodoList extends Component {
           />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default TodoList 
+export default TodoList
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,4 @@
-import {CHANGE_INPUT, ADD_ITEM, DELETE_ITEM, GET_LIST} from './actionTypes'
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM, GET_LIST } from './actionTypes'
 
 const defaultState = {
   inputValue: 'Write Something',
@@ -9,10 +9,11 @@ const defaultState = {
  * 暴露出去一个方法函数,有两个参数：state 和 action。
  * state: 是整个项目中需要管理的数据信息。
  */
-// Reducer里只能接收state，不能改变state。
+
 /**
+ * 一定要注意： Reducer里只能接收state，不能改变state。
  * 不要认为把业务逻辑写在了Reducer中，那改变state值的一定是Reducer。
- * 其实不然，Reudcer只是返回了更改的数据，但是并没有更改store中的数据，store拿到了Reducer的数据，自己对自己进行了更新。
+ * 其实不然，Reudcer只是返回了更改的数据，操作的是newState，但是并没有更改store中的state数据，store拿到了Reducer的数据，自己对自己进行了更新。
  */
 export default (state = defaultState, action) => {
   if (action.type === CHANGE_INPUT) {
@@ -31,12 +32,10 @@ export default (state = defaultState, action) => {
     newState.list.splice(action.index, 1) // 删除数组中对应的值
     return newState
   }
-  if(action.type === GET_LIST){
+  if (action.type === GET_LIST) {
     let newState = JSON.parse(JSON.stringify(state))
-    newState.list = action.data.data.list // 复制新的list数组进去
-    console.log(newState)
+    newState.list = action.data.list // 复制新的list数组进去
     return newState
   }
-  console.log(state)
   return state
 }
